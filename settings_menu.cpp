693c546d10204887370ff8f5f6e6407ee1e723b4
João Torezan <jabtorezan@gmail.com>
2024-12-14 17:38:30 -0300

    added library for settings menu

---
+++ settings_menu.cpp
@@ -0,0 +1,5 @@
+#include "settings_menu.h"
+
+SettingsMenu::SettingsMenu() {}
+
+
---
+++ settings_menu.h
@@ -0,0 +1,10 @@
+#ifndef Settings_MENU_H
+#define Settings_MENU_H
+
+class SettingsMenu {
+    public:
+        SettingsMenu();
+    private:
+};
+
+#endif
